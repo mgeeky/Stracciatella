90f3ae5bd63533d326092fc9704a432ebcf4a134
mgeeky <mb@binary-offensive.com>
2020-03-21 02:29:39 +0100

    Fixed encoder's raw bytes issue.

--- encoder.py
+++ encoder.py
@@ -18,6 +18,7 @@ def parseOptions(argv):
     parser.add_argument('command', nargs='?', help = 'Specifies either a command to encode or script file\'s path')
     parser.add_argument('-b', '--base64', action='store_true', help = 'Consider input as Base64 encoded. If both options, --base64 and --xor are specified, the program will apply them accordingly: Base64Encode(XorEncode(data, XorKey))')
     parser.add_argument('-x', '--xor', dest='xor', metavar = 'KEY', default = '0x00', type=str, help = 'Consider input as Base64 encoded. If both options, --base64 and --xor are specified, the program will apply them accordingly: Base64Encode(XorEncode(data, XorKey))')
+    parser.add_argument('-o', '--output', dest='output', metavar = 'PATH', type=str, help = '(optional) Output file. If not given - will echo output to stdout')
 
     opts = parser.parse_args()
 
@@ -45,7 +46,7 @@ def getData():
     my_file = Path(config['command'])
     try:
         if my_file.is_file():
-            with open(my_file) as f:
+            with open(my_file, 'rb') as f:
                 return f.read()
     except:
         pass
@@ -57,9 +58,9 @@ def base64Encode(x):
 
 def xorEncode(data, key):
     xored = []
-    for i, byte in enumerate(data):
+    for byte in data:
         xored.append(byte ^ ord(key))
-    return bytearray(xored)
+    return bytearray(xored)[:len(data)]
 
 def main(argv):
     (opts) = parseOptions(argv)
@@ -67,7 +68,7 @@ def main(argv):
         print('Options parsing failed.')
         return False
 
-    data = getData().encode()
+    data = getData()
     out = data
 
     if config['xorkey'] != 0:
@@ -76,7 +77,13 @@ def main(argv):
     if config['base64']:
         out = base64Encode(out)
 
-    sys.stdout.write(out.decode())
+    if (opts.output):
+        with open(opts.output, 'wb') as f:
+            f.write(out)
+
+        print('[+] Written {} bytes to: {}'.format(len(out), opts.output))
+    else:
+        sys.stdout.write(out.decode())
 
 if __name__ == '__main__':
     main(sys.argv)
